b4586c5f1404825e24220b86bcef3e69b0c908ee
seamon <seamon@sina.com>
2020-05-26 16:17:16 +0800

    trim leading zero for signature r and s
    sending raw transaction with signature where a leading zero <<0>> is in r or s will get an error:
    
    rlp: non-canonical integer (leading zero bytes) for *big.Int, decoding into (types.Transaction)(types.txdata).R
    
    so trim the leading zero for the signature r and s directly would be a good solution.

--- lib/eth/transaction/signer.ex
+++ lib/eth/transaction/signer.ex
@@ -124,7 +124,10 @@ defmodule ETH.Transaction.Signer do
 
     sig_v = if chain_id > 0, do: initial_v + (chain_id * 2 + 8), else: initial_v
 
-    [nonce, gas_price, gas_limit, to, value, data, <<sig_v>>, sig_r, sig_s]
+    [nonce, gas_price, gas_limit, to, value, data, <<sig_v>>, trim_leading_zero(sig_r), trim_leading_zero(sig_s)]
     |> ExRLP.encode()
   end
+
+  defp trim_leading_zero(<<0>> <> r_or_s), do: r_or_s
+  defp trim_leading_zero(r_or_s), do: r_or_s
 end
