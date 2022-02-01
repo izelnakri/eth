2c19416aac5026a67e7a34cda2c6d738881a3d12
Ethan Bergeron <52430539+berg95@users.noreply.github.com>
2022-01-31 23:55:19 -0600

    Update wallet.ex
    Implemented use of pipes

--- lib/eth/wallet.ex
+++ lib/eth/wallet.ex
@@ -5,12 +5,13 @@ defmodule ETH.Wallet do
   def create(private_key \\ :crypto.strong_rand_bytes(32))
 
   def create(<<encoded_private_key::binary-size(64)>>) do
-    public_key = get_public_key(encoded_private_key)
-    eth_address = get_address(public_key)
+    eth_address = encoded_private_key
+      |> get_public_key()
+      |> get_address()
 
     %{
       private_key: encoded_private_key,
-      public_key: Base.encode16(public_key),
+      public_key: Base.encode16(get_public_key(encoded_private_key)),
       eth_address: eth_address,
       mnemonic_phrase: Mnemonic.entropy_to_mnemonic(encoded_private_key)
     }
