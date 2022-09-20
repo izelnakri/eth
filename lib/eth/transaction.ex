3bf5c4a20aa8d16d8dc20e83d6b2712566444397
baxterjfinch <cryptohemlock@protonmail.com>
2022-09-20 14:17:36 -0700

    removing broken |> chain

--- lib/eth/transaction.ex
+++ lib/eth/transaction.ex
@@ -24,12 +24,11 @@ defmodule ETH.Transaction do
   end
 
   def send_transaction(params, private_key) do
-    tx_to_sign = set_default_from(params, private_key)
-      |> build
-    tx_to_sign
-      |> sign_transaction(private_key)
-      |> Base.encode16()
-      |> send
+    set_default_from(params, private_key)
+    |> build
+    |> sign_transaction(private_key)
+    |> Base.encode16()
+    |> send
   end
 
   def send_transaction(sender_wallet, receiver_wallet, value) when is_number(value) do
